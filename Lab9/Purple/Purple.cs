907e6af75f077f402bc983e9dbcd8afa5b193d47
Заболотный Кирилл <88078893+ovosh2281337@users.noreply.github.com>
2026-05-10 15:24:25 +0300

    Add files via upload

---
+++ Lab9/Purple/Purple.cs
@@ -0,0 +1,39 @@
+using System;
+
+public abstract class Purple
+{
+    private string _input;
+
+    public string Input
+    {
+        get { return _input; }
+    }
+
+    protected Purple(string text)
+    {
+        if (text == null)
+        {
+            _input = "";
+        }
+        else
+        {
+            _input = text;
+        }
+    }
+
+    public abstract void Review();
+
+    public virtual void ChangeText(string text)
+    {
+        if (text == null)
+        {
+            _input = "";
+        }
+        else
+        {
+            _input = text;
+        }
+
+        Review();
+    }
+}
