6dde32145afaa77a9760bb5f696ae90f5e70bbbf
Заболотный Кирилл <88078893+ovosh2281337@users.noreply.github.com>
2026-05-10 15:13:45 +0300

    Add files via upload

--- Lab10Test/Purple/GeneralTest.cs
+++ Lab10Test/Purple/GeneralTest.cs
@@ -1,260 +1,260 @@
-﻿//using Lab10;
-//using System.IO;
+﻿using Lab10;
+using System.IO;
 
-//namespace Lab10Test.Purple
-//{
-//	[TestClass]
-//	public sealed class GeneralTest
-//	{
-//		private class TestManager : Lab10.Purple.PurpleFileManager<Lab9.Purple.Purple>
-//		{
-//			public TestManager(string name) : base(name) { }
+namespace Lab10Test.Purple
+{
+	[TestClass]
+	public sealed class GeneralTest
+	{
+		private class TestManager : Lab10.Purple.PurpleFileManager<Lab9.Purple.Purple>
+		{
+			public TestManager(string name) : base(name) { }
 
-//			public TestManager(string name, string folder, string fileName, string ext = "txt")
-//				: base(name, folder, fileName, ext) { }
+			public TestManager(string name, string folder, string fileName, string ext = "txt")
+				: base(name, folder, fileName, ext) { }
 
-//			public override void Serialize(Lab9.Purple.Purple obj) { }
-//			public override Lab9.Purple.Purple Deserialize() => null;
-//		}
+			public override void Serialize(Lab9.Purple.Purple obj) { }
+			public override Lab9.Purple.Purple Deserialize() => null;
+		}
 
-//		[TestMethod]
-//		public void Test_00_OOP_IFileManager()
-//		{
-//			var fileManagerInterface = typeof(IFileManager);
-//			Assert.IsTrue(fileManagerInterface.IsInterface);
-//			Assert.IsNotNull(fileManagerInterface.GetProperty("FolderPath"));
-//			Assert.IsNotNull(fileManagerInterface.GetProperty("FileName"));
-//			Assert.IsNotNull(fileManagerInterface.GetProperty("FileExtension"));
-//			Assert.IsNotNull(fileManagerInterface.GetProperty("FullPath"));
-//			Assert.IsNotNull(fileManagerInterface.GetMethod("SelectFolder", new[] { typeof(string) }));
-//			Assert.IsNotNull(fileManagerInterface.GetMethod("ChangeFileName", new[] { typeof(string) }));
-//			Assert.IsNotNull(fileManagerInterface.GetMethod("ChangeFileFormat", new[] { typeof(string) }));
-//		}
+		[TestMethod]
+		public void Test_00_OOP_IFileManager()
+		{
+			var fileManagerInterface = typeof(IFileManager);
+			Assert.IsTrue(fileManagerInterface.IsInterface);
+			Assert.IsNotNull(fileManagerInterface.GetProperty("FolderPath"));
+			Assert.IsNotNull(fileManagerInterface.GetProperty("FileName"));
+			Assert.IsNotNull(fileManagerInterface.GetProperty("FileExtension"));
+			Assert.IsNotNull(fileManagerInterface.GetProperty("FullPath"));
+			Assert.IsNotNull(fileManagerInterface.GetMethod("SelectFolder", new[] { typeof(string) }));
+			Assert.IsNotNull(fileManagerInterface.GetMethod("ChangeFileName", new[] { typeof(string) }));
+			Assert.IsNotNull(fileManagerInterface.GetMethod("ChangeFileFormat", new[] { typeof(string) }));
+		}
 
-//		[TestMethod]
-//		public void Test_01_OOP_IFileLifeController()
-//		{
-//			var lifeInterface = typeof(IFileLifeController);
-//			Assert.IsTrue(lifeInterface.IsInterface);
-//			Assert.IsNotNull(lifeInterface.GetMethod("CreateFile", Type.EmptyTypes));
-//			Assert.IsNotNull(lifeInterface.GetMethod("EditFile", new[] { typeof(string) }));
-//			Assert.IsNotNull(lifeInterface.GetMethod("ChangeFileExtension", new[] { typeof(string) }));
-//			Assert.IsNotNull(lifeInterface.GetMethod("DeleteFile", Type.EmptyTypes));
-//		}
+		[TestMethod]
+		public void Test_01_OOP_IFileLifeController()
+		{
+			var lifeInterface = typeof(IFileLifeController);
+			Assert.IsTrue(lifeInterface.IsInterface);
+			Assert.IsNotNull(lifeInterface.GetMethod("CreateFile", Type.EmptyTypes));
+			Assert.IsNotNull(lifeInterface.GetMethod("EditFile", new[] { typeof(string) }));
+			Assert.IsNotNull(lifeInterface.GetMethod("ChangeFileExtension", new[] { typeof(string) }));
+			Assert.IsNotNull(lifeInterface.GetMethod("DeleteFile", Type.EmptyTypes));
+		}
 
-//		[TestMethod]
-//		public void Test_02_OOP_ISerializer_T()
-//		{
-//			var serializerInterface = typeof(ISerializer<>);
-//			Assert.IsTrue(serializerInterface.IsInterface, "ISerializer<T> must be an interface");
+		[TestMethod]
+		public void Test_02_OOP_ISerializer_T()
+		{
+			var serializerInterface = typeof(ISerializer<>);
+			Assert.IsTrue(serializerInterface.IsInterface, "ISerializer<T> must be an interface");
 
-//			var serializeMethod = serializerInterface.GetMethod("Serialize");
-//			Assert.IsNotNull(serializeMethod, "Serialize method is missing in ISerializer<T>");
-//			Assert.AreEqual(1, serializeMethod.GetParameters().Length, "Serialize must take one parameter");
+			var serializeMethod = serializerInterface.GetMethod("Serialize");
+			Assert.IsNotNull(serializeMethod, "Serialize method is missing in ISerializer<T>");
+			Assert.AreEqual(1, serializeMethod.GetParameters().Length, "Serialize must take one parameter");
 
-//			var deserializeMethod = serializerInterface.GetMethod("Deserialize");
-//			Assert.IsNotNull(deserializeMethod, "Deserialize method is missing in ISerializer<T>");
-//			Assert.AreEqual(0, deserializeMethod.GetParameters().Length, "Deserialize must take no parameters");
-//		}
+			var deserializeMethod = serializerInterface.GetMethod("Deserialize");
+			Assert.IsNotNull(deserializeMethod, "Deserialize method is missing in ISerializer<T>");
+			Assert.AreEqual(0, deserializeMethod.GetParameters().Length, "Deserialize must take no parameters");
+		}
 
-//		[TestMethod]
-//		public void Test_03_OOP_MyFileManager()
-//		{
-//			var managerType = typeof(MyFileManager);
+		[TestMethod]
+		public void Test_03_OOP_MyFileManager()
+		{
+			var managerType = typeof(MyFileManager);
 
-//			Assert.IsTrue(managerType.IsClass, "MyFileManager must be a class");
-//			Assert.IsTrue(managerType.IsAbstract, "MyFileManager must be abstract");
+			Assert.IsTrue(managerType.IsClass, "MyFileManager must be a class");
+			Assert.IsTrue(managerType.IsAbstract, "MyFileManager must be abstract");
 
-//			Assert.IsNotNull(managerType.GetConstructor(new[] { typeof(string) }),
-//				"Constructor MyFileManager(string name) is missing");
+			Assert.IsNotNull(managerType.GetConstructor(new[] { typeof(string) }),
+				"Constructor MyFileManager(string name) is missing");
 
-//			Assert.IsNotNull(managerType.GetConstructor(
-//				new[] { typeof(string), typeof(string), typeof(string), typeof(string) }),
-//				"Constructor MyFileManager(string, string, string, string) is missing");
+			Assert.IsNotNull(managerType.GetConstructor(
+				new[] { typeof(string), typeof(string), typeof(string), typeof(string) }),
+				"Constructor MyFileManager(string, string, string, string) is missing");
 
-//			var nameProp = managerType.GetProperty("Name");
-//			Assert.IsNotNull(nameProp, "Property 'Name' is missing");
-//			Assert.IsTrue(nameProp.CanRead, "Name must have getter");
-//            Assert.IsTrue(!nameProp.CanWrite || !nameProp.SetMethod!.IsPublic, "Name should be read-only");
+			var nameProp = managerType.GetProperty("Name");
+			Assert.IsNotNull(nameProp, "Property 'Name' is missing");
+			Assert.IsTrue(nameProp.CanRead, "Name must have getter");
+           Assert.IsTrue(!nameProp.CanWrite || !nameProp.SetMethod!.IsPublic, "Name should be read-only");
 
-//            Assert.IsTrue(typeof(IFileManager).IsAssignableFrom(managerType));
-//			Assert.IsTrue(typeof(IFileLifeController).IsAssignableFrom(managerType));
-//		}
+           Assert.IsTrue(typeof(IFileManager).IsAssignableFrom(managerType));
+			Assert.IsTrue(typeof(IFileLifeController).IsAssignableFrom(managerType));
+		}
 
-//		[TestMethod]
-//		public void Test_04_OOP_PurpleFileManager()
-//		{
-//			var PurpleType = typeof(Lab10.Purple.PurpleFileManager<>);
-//			var myFileManagerType = typeof(MyFileManager);
+		[TestMethod]
+		public void Test_04_OOP_PurpleFileManager()
+		{
+			var PurpleType = typeof(Lab10.Purple.PurpleFileManager<>);
+			var myFileManagerType = typeof(MyFileManager);
 
-//			Assert.IsTrue(PurpleType.IsClass, "PurpleFileManager<T> must be a class");
-//			Assert.IsTrue(PurpleType.IsAbstract, "PurpleFileManager<T> must be abstract");
-//			Assert.IsTrue(PurpleType.IsSubclassOf(myFileManagerType),
-//				"PurpleFileManager<T> must inherit from MyFileManager");
+			Assert.IsTrue(PurpleType.IsClass, "PurpleFileManager<T> must be a class");
+			Assert.IsTrue(PurpleType.IsAbstract, "PurpleFileManager<T> must be abstract");
+			Assert.IsTrue(PurpleType.IsSubclassOf(myFileManagerType),
+				"PurpleFileManager<T> must inherit from MyFileManager");
 
-//			var implementedInterfaces = PurpleType.GetInterfaces();
-//			Assert.IsTrue(implementedInterfaces.Any(i =>
-//				i.IsGenericType &&
-//				i.GetGenericTypeDefinition() == typeof(ISerializer<>)),
-//				"PurpleFileManager<T> must implement ISerializer<T>");
+			var implementedInterfaces = PurpleType.GetInterfaces();
+			Assert.IsTrue(implementedInterfaces.Any(i =>
+				i.IsGenericType &&
+				i.GetGenericTypeDefinition() == typeof(ISerializer<>)),
+				"PurpleFileManager<T> must implement ISerializer<T>");
 
-//			var serialize = PurpleType.GetMethod("Serialize");
-//			Assert.IsNotNull(serialize, "Serialize method is missing");
-//			Assert.IsTrue(serialize.IsAbstract, "Serialize must be abstract");
+			var serialize = PurpleType.GetMethod("Serialize");
+			Assert.IsNotNull(serialize, "Serialize method is missing");
+			Assert.IsTrue(serialize.IsAbstract, "Serialize must be abstract");
 
-//			var deserialize = PurpleType.GetMethod("Deserialize");
-//			Assert.IsNotNull(deserialize, "Deserialize method is missing");
-//			Assert.IsTrue(deserialize.IsAbstract, "Deserialize must be abstract");
+			var deserialize = PurpleType.GetMethod("Deserialize");
+			Assert.IsNotNull(deserialize, "Deserialize method is missing");
+			Assert.IsTrue(deserialize.IsAbstract, "Deserialize must be abstract");
 
-//			var edit = PurpleType.GetMethod("EditFile", new[] { typeof(string) });
-//			Assert.IsNotNull(edit, "EditFile must be overridden");
-//			Assert.IsTrue(edit.IsVirtual, "EditFile must be virtual");
-//			Assert.IsFalse(edit.IsAbstract, "EditFile must not be abstract");
-//			Assert.AreEqual(PurpleType, edit.DeclaringType);
+			var edit = PurpleType.GetMethod("EditFile", new[] { typeof(string) });
+			Assert.IsNotNull(edit, "EditFile must be overridden");
+			Assert.IsTrue(edit.IsVirtual, "EditFile must be virtual");
+			Assert.IsFalse(edit.IsAbstract, "EditFile must not be abstract");
+			Assert.AreEqual(PurpleType, edit.DeclaringType);
 
-//			var changeExt = PurpleType.GetMethod("ChangeFileExtension", new[] { typeof(string) });
-//			Assert.IsNotNull(changeExt, "ChangeFileExtension must be overridden");
-//			Assert.IsTrue(changeExt.IsVirtual, "ChangeFileExtension must be virtual");
-//			Assert.IsFalse(changeExt.IsAbstract, "ChangeFileExtension must not be abstract");
-//			Assert.AreEqual(PurpleType, changeExt.DeclaringType);
-//		}
+			var changeExt = PurpleType.GetMethod("ChangeFileExtension", new[] { typeof(string) });
+			Assert.IsNotNull(changeExt, "ChangeFileExtension must be overridden");
+			Assert.IsTrue(changeExt.IsVirtual, "ChangeFileExtension must be virtual");
+			Assert.IsFalse(changeExt.IsAbstract, "ChangeFileExtension must not be abstract");
+			Assert.AreEqual(PurpleType, changeExt.DeclaringType);
+		}
 
-//		[TestMethod]
-//		public void Test_05_FileManager_Setup()
-//		{
-//			var manager = (IFileManager)new TestManager("test");
-//			var folder = Directory.GetCurrentDirectory();
-//			manager.SelectFolder(folder);
-//			manager.ChangeFileName("task");
-//			Assert.AreEqual(folder, manager.FolderPath);
-//			Assert.AreEqual("task", manager.FileName);
-//			Assert.IsTrue(manager.FullPath.Contains("task"));
-//		}
+		[TestMethod]
+		public void Test_05_FileManager_Setup()
+		{
+			var manager = (IFileManager)new TestManager("test");
+			var folder = Directory.GetCurrentDirectory();
+			manager.SelectFolder(folder);
+			manager.ChangeFileName("task");
+			Assert.AreEqual(folder, manager.FolderPath);
+			Assert.AreEqual("task", manager.FileName);
+			Assert.IsTrue(manager.FullPath.Contains("task"));
+		}
 
-//		[TestMethod]
-//		public void Test_06_FileCreation()
-//		{
-//			var manager = (IFileManager)new TestManager("test");
-//			var folder = Directory.GetCurrentDirectory();
-//			manager.SelectFolder(folder);
-//			manager.ChangeFileName("task");
-//			((IFileLifeController)manager).CreateFile();
-//			Assert.IsTrue(File.Exists(manager.FullPath));
-//		}
+		[TestMethod]
+		public void Test_06_FileCreation()
+		{
+			var manager = (IFileManager)new TestManager("test");
+			var folder = Directory.GetCurrentDirectory();
+			manager.SelectFolder(folder);
+			manager.ChangeFileName("task");
+			((IFileLifeController)manager).CreateFile();
+			Assert.IsTrue(File.Exists(manager.FullPath));
+		}
 
-//		[TestMethod]
-//		public void Test_07_ChangeFileFormat()
-//		{
-//			var manager = (IFileManager)new TestManager("test");
-//			var folder = Path.Combine(Directory.GetCurrentDirectory(), "PurpleFormatTest");
-//			Directory.CreateDirectory(folder);
-//			manager.SelectFolder(folder);
-//			manager.ChangeFileName("task");
-//			manager.ChangeFileFormat("json");
-//			Assert.AreEqual("json", manager.FileExtension);
-//			Assert.IsTrue(File.Exists(manager.FullPath));
-//			Directory.Delete(folder, true);
-//		}
+		[TestMethod]
+		public void Test_07_ChangeFileFormat()
+		{
+			var manager = (IFileManager)new TestManager("test");
+			var folder = Path.Combine(Directory.GetCurrentDirectory(), "PurpleFormatTest");
+			Directory.CreateDirectory(folder);
+			manager.SelectFolder(folder);
+			manager.ChangeFileName("task");
+			manager.ChangeFileFormat("json");
+			Assert.AreEqual("json", manager.FileExtension);
+			Assert.IsTrue(File.Exists(manager.FullPath));
+			Directory.Delete(folder, true);
+		}
 
-//		[TestMethod]
-//		public void Test_08_EditFile()
-//		{
-//			var manager = new TestManager("test");
-//			var fileManager = (IFileManager)manager;
-//			var folder = Path.Combine(Directory.GetCurrentDirectory(), "PurpleEditTest");
-//			Directory.CreateDirectory(folder);
+		[TestMethod]
+		public void Test_08_EditFile()
+		{
+			var manager = new TestManager("test");
+			var fileManager = (IFileManager)manager;
+			var folder = Path.Combine(Directory.GetCurrentDirectory(), "PurpleEditTest");
+			Directory.CreateDirectory(folder);
 
-//			fileManager.SelectFolder(folder);
-//			fileManager.ChangeFileName("task");
-//			manager.CreateFile();
-//			manager.EditFile("HELLO");
+			fileManager.SelectFolder(folder);
+			fileManager.ChangeFileName("task");
+			manager.CreateFile();
+			manager.EditFile("HELLO");
 
-//			var content = File.ReadAllText(fileManager.FullPath);
-//			Assert.AreEqual("HELLO", content);
+			var content = File.ReadAllText(fileManager.FullPath);
+			Assert.AreEqual("HELLO", content);
 
-//			Directory.Delete(folder, true);
-//		}
+			Directory.Delete(folder, true);
+		}
 
-//		[TestMethod]
-//		public void Test_09_ChangeFileExtension()
-//		{
-//			var manager = new TestManager("test");
-//			var fileManager = (IFileManager)manager;
-//			var folder = Path.Combine(Directory.GetCurrentDirectory(), "PurpleExtTest");
-//			Directory.CreateDirectory(folder);
+		[TestMethod]
+		public void Test_09_ChangeFileExtension()
+		{
+			var manager = new TestManager("test");
+			var fileManager = (IFileManager)manager;
+			var folder = Path.Combine(Directory.GetCurrentDirectory(), "PurpleExtTest");
+			Directory.CreateDirectory(folder);
 
-//			fileManager.SelectFolder(folder);
-//			fileManager.ChangeFileName("task");
-//			manager.CreateFile();
-//			manager.EditFile("DATA");
-//			manager.ChangeFileExtension("json");
+			fileManager.SelectFolder(folder);
+			fileManager.ChangeFileName("task");
+			manager.CreateFile();
+			manager.EditFile("DATA");
+			manager.ChangeFileExtension("json");
 
-//			Assert.AreEqual("json", fileManager.FileExtension);
-//			Assert.IsTrue(File.Exists(fileManager.FullPath));
+			Assert.AreEqual("json", fileManager.FileExtension);
+			Assert.IsTrue(File.Exists(fileManager.FullPath));
 
-//			var content = File.ReadAllText(fileManager.FullPath);
-//			Assert.AreEqual("DATA", content);
+			var content = File.ReadAllText(fileManager.FullPath);
+			Assert.AreEqual("DATA", content);
 
-//			Directory.Delete(folder, true);
-//		}
+			Directory.Delete(folder, true);
+		}
 
-//		[TestMethod]
-//		public void Test_10_DeleteFile()
-//		{
-//			var manager = new TestManager("test");
-//			var fileManager = (IFileManager)manager;
-//			var folder = Path.Combine(Directory.GetCurrentDirectory(), "PurpleDeleteTest");
-//			Directory.CreateDirectory(folder);
+		[TestMethod]
+		public void Test_10_DeleteFile()
+		{
+			var manager = new TestManager("test");
+			var fileManager = (IFileManager)manager;
+			var folder = Path.Combine(Directory.GetCurrentDirectory(), "PurpleDeleteTest");
+			Directory.CreateDirectory(folder);
 
-//			fileManager.SelectFolder(folder);
-//			fileManager.ChangeFileName("task");
-//			manager.CreateFile();
+			fileManager.SelectFolder(folder);
+			fileManager.ChangeFileName("task");
+			manager.CreateFile();
 
-//			var path = fileManager.FullPath;
-//			manager.DeleteFile();
+			var path = fileManager.FullPath;
+			manager.DeleteFile();
 
-//			Assert.IsFalse(File.Exists(path));
+			Assert.IsFalse(File.Exists(path));
 
-//			Directory.Delete(folder, true);
-//		}
+			Directory.Delete(folder, true);
+		}
 
-//		[TestMethod]
-//		public void Test_11_PurpleFileManager_Overrides()
-//		{
-//			var folder = Path.Combine(Directory.GetCurrentDirectory(), "PurpleOverrideTest");
-//			Directory.CreateDirectory(folder);
+		[TestMethod]
+		public void Test_11_PurpleFileManager_Overrides()
+		{
+			var folder = Path.Combine(Directory.GetCurrentDirectory(), "PurpleOverrideTest");
+			Directory.CreateDirectory(folder);
 
-//			try
-//			{
-//				var manager = new TestManager("test", folder, "task", "txt");
-//				var fileManager = (IFileManager)manager;
-//				var lifeController = (IFileLifeController)manager;
+			try
+			{
+				var manager = new TestManager("test", folder, "task", "txt");
+				var fileManager = (IFileManager)manager;
+				var lifeController = (IFileLifeController)manager;
 
-//				fileManager.SelectFolder(folder);
-//				fileManager.ChangeFileName("task");
+				fileManager.SelectFolder(folder);
+				fileManager.ChangeFileName("task");
 
-//				lifeController.CreateFile();
-//				lifeController.EditFile("Original Purple Content");
+				lifeController.CreateFile();
+				lifeController.EditFile("Original Purple Content");
 
-//				string originalPath = fileManager.FullPath;
+				string originalPath = fileManager.FullPath;
 
-//				lifeController.ChangeFileExtension("json");
+				lifeController.ChangeFileExtension("json");
 
-//				Assert.AreEqual("json", fileManager.FileExtension);
-//				Assert.IsTrue(File.Exists(fileManager.FullPath));
-//				Assert.IsFalse(File.Exists(originalPath));
+				Assert.AreEqual("json", fileManager.FileExtension);
+				Assert.IsTrue(File.Exists(fileManager.FullPath));
+				Assert.IsFalse(File.Exists(originalPath));
 
-//				var content = File.ReadAllText(fileManager.FullPath);
-//				Assert.AreEqual("Original Purple Content", content);
+				var content = File.ReadAllText(fileManager.FullPath);
+				Assert.AreEqual("Original Purple Content", content);
 
-//				lifeController.EditFile("Updated Purple Content");
-//				Assert.AreEqual("Updated Purple Content", File.ReadAllText(fileManager.FullPath));
-//			}
-//			finally
-//			{
-//				if (Directory.Exists(folder))
-//					Directory.Delete(folder, true);
-//			}
-//		}
-//	}
-//}
+				lifeController.EditFile("Updated Purple Content");
+				Assert.AreEqual("Updated Purple Content", File.ReadAllText(fileManager.FullPath));
+			}
+			finally
+			{
+				if (Directory.Exists(folder))
+					Directory.Delete(folder, true);
+			}
+		}
+	}
+}
